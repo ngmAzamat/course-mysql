bd1a8c120a619d1691e67416cff659fff08da098
Azamat <azamat@devbook.local>
2025-07-18 15:50:15 +0400

    commit

--- Project-TPKT/chat_web/app/controllers/messages_controller.rb
+++ Project-TPKT/chat_web/app/controllers/messages_controller.rb
@@ -5,6 +5,7 @@ class MessagesController < ApplicationController
           content: params[:content],
           client_id: cookies[:client_id]
         )
+        ActionCable.server.broadcast("chat", render_to_string(partial: "messages/message", locals: { message: @message }))
         redirect_to root_path
       end
   end
